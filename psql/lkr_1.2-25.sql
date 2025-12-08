4b427aac211a2e77eada9e192996cb7cb9659320
Alaedindouak <alaedin.hak@gmail.com>
2025-12-08 03:13:29 +0100

    finished numeric, floating-point section

--- psql/lkr_1.2-25.sql
+++ psql/lkr_1.2-25.sql
@@ -41,7 +41,7 @@ SELECT * FROM person;
 
 CREATE TABLE post(
    description TEXT,
-   interest_rate NUMERIC -- DECIMAL | numeric & decimal are the same
+   interest_rate NUMERIC -- decimal | numeric & decimal are the same
 );
 
 SELECT 123456798123456798123456798::BIGINT; -- This will cause an error because the number is out of range for BIGINT
@@ -57,3 +57,79 @@ SELECT 121.345::NUMERIC(5, 3); -- This will cause an error because total digits 
 SELECT 12.345::NUMERIC(5, 2); -- 12.35 
 SELECT 12122.345::NUMERIC(5); -- 12122
 SELECT 121221.345::NUMERIC(5, -2); -- 121200
+
+
+-- 
+
+drop table temperature_sensor;
+
+-- real - 4 bytes - 6 digits after decimal point of precision / alias float4 | float(1-24)
+-- double precision - 8 bytes - 15 digits after decimal point of precision | alias float8 | float(25-53)
+
+-- they're fast but less precise/they can loss precision (less precise than numeric/decimal)
+
+create table temperature_sensor(
+   sensor_name text,
+   reading float(23)
+);
+
+select * from generate_series(1, 20) num;
+
+
+select
+   sum(num::double precision  / (num + 1))::double precision
+from 
+   generate_series(1, 5000000) num;
+
+select
+   sum(num::numeric / (num + 1))::numeric
+from 
+   generate_series(1, 5000000) num;
+
+
+--
+
+-- store meney in database
+-- money data type not recommended
+-- floating-points are not precise so they're out 
+-- so,
+
+-- 1. store money in integer (cents | lowest units in the currency)
+select (100.99 * 100)::int4 as cents;
+select (100.99 * 10000)::int4 as thousandth_cents;
+
+-- 2. store money in numeric/decimal
+select (100.99)::numeric as price;
+select (100000.99)::numeric(10, 4) as price;
+select (10000000.99)::numeric(10, 2) as price;
+
+--
+
+-- NaN
+-- Infinity | Inf
+
+-- 
+-- casting
+
+select 100::money;
+
+select cast(100 as money);
+
+select pg_typeof(100::int4);
+select pg_typeof(100::float(43));
+
+select pg_typeof(100::int8) as bigint, pg_typeof(cast(100 as int4)) as integer;
+
+select integer '100';
+
+select int8 '100';
+select pg_typeof(int8 '100');
+
+select 
+   pg_column_size(100::int2),
+   pg_column_size(100::int4),
+   pg_column_size(100::int8),
+   pg_column_size(1214321.21333::numeric);
+
+--
+
